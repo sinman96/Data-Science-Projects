c23c378492036fbfa684f3c543d1dc56d2015a52
unknown <samuelinman104@yahoo.com>
2022-08-19 17:33:28 +0100

    Incorporated XGBoost into each model

--- DATASCIENCEMLCOURSE/RussianExports.py
+++ DATASCIENCEMLCOURSE/RussianExports.py
@@ -23,7 +23,7 @@ dataprocessor(dataset)
 #Dropping redundant data fields that don't need to be used in modelling
 dataset = dataset.drop(['Classification','index','Partner Code',
 'Partner ISO','Reporter Code','Reporter',
-'Reporter ISO','Qty Unit Code'], axis = 1)
+'Reporter ISO','Qty Unit Code', 'Qty Unit','Commodity'], axis = 1)
 #Reduce project into data entries with a trade (Qty > 0) and EU countries 
 dataset = dataset[dataset.Qty != 0.0]
 EU_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 
@@ -40,23 +40,26 @@ print(dataset.nunique())
 print(dataset.values[1000])
 #Find the amount traded in exports with each country
 pre_georgia_invasion_dataset = dataset[dataset['Year'] == 2007]
-post_georgia_invasion_dataset = dataset[(dataset['Year']>= 2009) 
-                                        & (dataset['Year']<= 2013)]
-post_crimea_invasion_dataset = dataset[dataset['Year']>= 2015]
+
+post_georgia_invasion_dataset = dataset[(dataset['Year'] >= 2009) 
+                                        & (dataset['Year'] <= 2013)]
+post_crimea_invasion_dataset = dataset[dataset['Year'] >= 2015]
 
 #Displaying findings from the data over these two time periods
 euexportinformation(pre_georgia_invasion_dataset
                     , EU_countries, 'Pre Georgia Invasion')
-euexportinformation(pre_georgia_invasion_dataset
+euexportinformation(post_georgia_invasion_dataset
                     , EU_countries, 'Pre Crimea/post Georgia invasion')
 euexportinformation(post_crimea_invasion_dataset
                     , EU_countries, 'Post Crimea invasion')
 
-
-
-
-
-
-
+#To display the ordering of the fields
+print("First data entry")
+#From these we can see what indexes of categorical fields
+print(dataset.iloc[0])
+#
+ModelAccuracy(pre_georgia_invasion_dataset)
+ModelAccuracy(post_georgia_invasion_dataset)
+ModelAccuracy(post_crimea_invasion_dataset)
 
 
--- DATASCIENCEMLCOURSE/RussianExportsFunctions.py
+++ DATASCIENCEMLCOURSE/RussianExportsFunctions.py
@@ -39,7 +39,6 @@ def euexportinformation(data, datasubset, time_period):
     export_total = sum(export_totals)
     for i in range(0, len(export_totals)):
         export_totals[i] /= export_total
-    print(export_totals)
     #Weighted percentages of trade with each country over this time
     most_traded_countries = []
     for i in range(0, len(datasubset)):
@@ -51,3 +50,40 @@ def euexportinformation(data, datasubset, time_period):
     plt.title('Russian EU export partners ' + time_period, fontsize = 20)
     plt.legend(loc ='lower left')
     return export_totals
+
+def ModelAccuracy(data):
+    #Default predictive data split
+    X1 = data.iloc[:, :2].values
+    X2 = data.iloc[:, 3:].values
+    X = con = np.concatenate((X1, X2), axis=1)
+    y = data.iloc[:, 2].values
+    #Only non numeric fields are Partner and Commodity, so those will be encoded
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import OneHotEncoder
+    ct1 = ColumnTransformer(transformers=[('encoder',
+    OneHotEncoder(sparse = False), [2])], remainder='passthrough')
+    X = np.array(ct1.fit_transform(X))
+    print(X)
+
+    """## Splitting the dataset into the Training set and Test set"""
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+    test_size = 0.25, random_state = 0)
+ 
+
+    from xgboost import XGBClassifier
+    classifier = XGBClassifier()
+    classifier.fit(X_train, y_train)
+    """## Making the Confusion Matrix"""
+
+    from sklearn.metrics import confusion_matrix, accuracy_score
+    y_pred = classifier.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    print(accuracy_score(y_test, y_pred))
+
+    """## Applying K-Fold Cross Validation##"""
+    from sklearn.model_selection import cross_val_score
+    accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+    print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+    print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
