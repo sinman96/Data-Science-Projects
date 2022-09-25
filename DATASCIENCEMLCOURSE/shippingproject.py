e2b54b41135fa68ba0e95713d29c7c49d9854027
unknown <samuelinman104@yahoo.com>
2022-09-25 18:10:11 +0100

    Separated data by IN and BD

--- DATASCIENCEMLCOURSE/shippingproject.py
+++ DATASCIENCEMLCOURSE/shippingproject.py
@@ -8,7 +8,7 @@ Created on Wed Aug 31 15:15:14 2022
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from ShippingProjectFunctions import *
+from shippingprojectfunctions import *
 
 """## Importing the dataset"""
 #People have split this dataset but I'll merge it resplit it to
@@ -32,68 +32,28 @@ dataset = dataset.drop(['pick_up_point','source_country',
 dataset = dataset.sort_values(by='send_timestamp', ascending=True)
 print(len(dataset))
 #See if the only two drop off points correspond to the two countries
-print((dataset[(dataset['drop_off_point'] == "Y")
-               & (dataset['destination_country'] == "BD")]))
-print((dataset[(dataset['drop_off_point'] == "X")
-               & (dataset['destination_country'] == "IN")]))  
+print(len((dataset[(dataset['drop_off_point'] == "Y")
+               & (dataset['destination_country'] == "BD")])))
+print((len(dataset[(dataset['drop_off_point'] == "X")
+               & (dataset['destination_country'] == "IN")])))  
 #From this we can tell all drop+off points X are in Bangladesh and all drop off 
 #points Y are in India
-dataset = dataset.drop(['drop_off_point'], axis = 1)
 print(dataset)
+print(dataset.columns)
+regression(dataset['gross_weight'].values, dataset['freight_cost'].values)
+dataset['shipping_cost_in($)'] = dataset['freight_cost'] + dataset['gross_weight']*dataset['shipment_charges']
+print(dataset)
+print(dataset.columns)
+dataset = dataset.drop(['freight_cost','shipment_charges',], axis = 1)
+print(dataset)
+print(dataset.columns)
 
+India = dataset[dataset.destination_country == 'IN']
+India = India.drop(['drop_off_point','destination_country'], axis = 1)
+print(India)
+print(India.columns)
 
-
-#Separate dataset into datasets for the three different shipping companies
-SC1 = dataset[(dataset['shipping_company'] == "SC1")]
-SC1['shipment_mode'].unique()
-#We can see that SC1 only has its shipping mode as Ocean we'll drop it,
-SC1 = SC1.drop(['shipment_mode', 'shipping_company'], axis = 1)
-SC1_India = SC1[(SC1['destination_country'] == "IN")]
-SC1_India = SC1_India.drop(['destination_country'], axis = 1)
-SC1_India = SC1_India.sort_values(by='send_timestamp', ascending = False)
-print(SC1_India.nunique())
-SC1_India_Ocean = SC1_India
-SC1_Bangladesh = SC1[(SC1['destination_country'] == "BD")]
-SC1_Bangladesh = SC1_Bangladesh.drop(['destination_country'], axis = 1)
-SC1_Bangladesh = SC1_Bangladesh.sort_values(by='send_timestamp', ascending = False)
-print(SC1_Bangladesh.nunique())
-SC1_Bangladesh_Ocean = SC1_Bangladesh
-
-
-SC2 = dataset[(dataset['shipping_company'] == "SC2")]
-SC2['shipment_mode'].unique()
-#We won't drop shipment mode here as it has Air and Ocean entries.
-SC2 = SC2.drop(['shipping_company'], axis = 1)
-SC2_India = SC2[(SC2['destination_country'] == "IN")]
-SC2_India = SC2_India.drop(['destination_country'], axis = 1)
-SC2_India = SC2_India.sort_values(by='send_timestamp', ascending = False)
-print(SC2_India.nunique())
-print(SC2_India['shipment_mode'].unique())
-SC2_India = SC2_India.drop(['shipment_mode'], axis = 1)
-print(SC2_India.nunique())
-SC2_India_Air = SC2_India
-
-SC2_Bangladesh = SC2[(SC2['destination_country'] == "BD")]
-SC2_Bangladesh = SC2_Bangladesh.drop(['destination_country'], axis = 1)
-SC2_Bangladesh = SC2_Bangladesh.sort_values(by='send_timestamp', ascending = False)
-print(SC2_Bangladesh.nunique())
-print(SC2_Bangladesh['shipment_mode'].unique())
-SC2_Bangladesh = SC2_Bangladesh.drop(['shipment_mode'], axis = 1)
-print(SC2_Bangladesh.nunique())
-SC2_Bangladesh_Ocean = SC2_Bangladesh
-
-
-SC3 = dataset[(dataset['shipping_company'] == "SC3")]
-SC3['shipment_mode'].unique()
-#We can see that SC1 only has its shipping mode as Ocean we'll drop it,
-SC3 = SC3.drop(['shipment_mode', 'shipping_company'], axis = 1)
-SC3_India = SC3[(SC3['destination_country'] == "IN")]
-SC3_India = SC3_India.drop(['destination_country'], axis = 1)
-SC3_India = SC3_India.sort_values(by='send_timestamp', ascending = False)
-print(SC3_India.nunique())
-SC3_India_Ocean = SC3_India
-#After Inspection SC3_Bangladesh is empty, so we'll just focus on the others.
-
-
-
-
+Bangladesh = dataset[dataset.destination_country == 'BD']
+Bangladesh = Bangladesh.drop(['drop_off_point','destination_country'], axis = 1)
+print(Bangladesh)
+print(Bangladesh.columns)
